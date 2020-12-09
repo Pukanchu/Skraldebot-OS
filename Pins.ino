9093dd135f6a7c5baa2304eb2acb6a980682f3d7
Porno-per-69 <75021386+Porno-per-69@users.noreply.github.com>
2020-12-09 12:25:57 +0100

    Update Pins.ino

--- Pins.ino
+++ Pins.ino
@@ -1,1 +1,16 @@
 
+/*
+//højre moter
+#define AIN1 2;
+#define BIN1 4;
+
+//vænstre moter
+#define AIN2 7;
+#define BIN2 8;
+
+//hastighedskontrol
+#define PMWA 9;
+#define PMWB 10;
+
+#define STBY 12;
+*/
